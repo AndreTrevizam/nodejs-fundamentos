377a6def5f8e3827f5dffddbeda7b41496611cf4
AndreTrevizam <andretrevizam264@gmail.com>
2026-03-10 22:17:55 -0300

    feat: Implement JSON body parsing middleware and update stream handling examples.

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
-    res.setHeader("Content-Type", "application/json");
     return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
+      name,
+      email,
       id: 1,
-      name: "John Doe",
     });
     return res.writeHead(201).end();
   }
